d3d251fde1d1143d7909a976dc6e308c05dd47d7
omixmaxdimo <67067757+omixmaxdimo@users.noreply.github.com>
2025-10-09 07:36:38 +0330

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .Chat.chat_OpenAI import OmixChatPro
+
+NODE_CLASS_MAPPINGS = {
+    "OmixChatPro": OmixChatPro
+
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "OmixChatPro": "👾 OpenAI Chat API🔑"
+}
