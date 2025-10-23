8349c92cbc17e9ecb52d8c2ab28f55bbf2884a0a
OmiX <67067757+omixmaxdimo@users.noreply.github.com>
2025-10-23 15:35:22 +0330

    Update __init__.py

--- Math/__init__.py
+++ Math/__init__.py
@@ -11,14 +11,3 @@ NODE_DISPLAY_NAME_MAPPINGS = {}
 # Add Resulation_Presets nodes
 NODE_CLASS_MAPPINGS.update(RES_PRESETS_NODES)
 NODE_DISPLAY_NAME_MAPPINGS.update(RES_PRESETS_DISPLAY)
-
-# === How to add more Math nodes in the future ===
-# 1. Create your new node file in this folder, e.g., MyNewMathNode.py
-# 2. Import it like this:
-#     from .MyNewMathNode import NODE_CLASS_MAPPINGS as NEW_NODE, NODE_DISPLAY_NAME_MAPPINGS as NEW_DISPLAY
-# 3. Update the dictionaries:
-#     NODE_CLASS_MAPPINGS.update(NEW_NODE)
-#     NODE_DISPLAY_NAME_MAPPINGS.update(NEW_DISPLAY)
-# 4. Done! ComfyUI will now see your new Math node automatically.
-
-# This structure keeps all Math nodes organized and easy to extend
